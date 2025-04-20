b97e8ad9314f9604ae9a28cc882dcc6308a2eb2e
SwapnilG2 <mrpc.windows@gmail.com>
2025-04-20 23:29:43 +0530

    add coomit

---
+++ src/app/components/landing/landing.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule} from '@angular/material/card';
+import { MatIconModule} from '@angular/material/icon';
+@Component({
+  selector: 'app-landing',
+  imports: [CommonModule, MatButtonModule, MatCardModule, MatIconModule],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.css',
+  standalone : true
+})
+
+export class LandingComponent {
+  features = [
+    {
+      icon: 'event_available',
+      title: 'Easy Scheduling',
+      description: 'Plan and schedule events with just a few clicks.'
+    },
+    {
+      icon: 'group',
+      title: 'Collaborate Easily',
+      description: 'Invite and manage attendees seamlessly.'
+    },
+    {
+      icon: 'insights',
+      title: 'Real-Time Insights',
+      description: 'Monitor progress with live reports and analytics.'
+    },
+    {
+      icon: 'security',
+      title: 'Secure Platform',
+      description: 'Your data is safe with our top-tier security.'
+    }
+  ];
+}
+
